a87495b576d5f1112f7579e203db2b5ecee858e6
Eric Stefano Silva Milhomem <94946025+Ericssm96@users.noreply.github.com>
2022-09-23 13:01:04 -0300

    Added more configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -8,5 +9,20 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js' // O arquivo final (de saída) vai ter o mesmo nome que tiver na propriedade do objeto atribuído à propriedade "entry", nesse caso se chamará "bundle"
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack App',
+            filename: 'index.html',
+            template: 'src/template.html'
+        }),
+    ]
 }
